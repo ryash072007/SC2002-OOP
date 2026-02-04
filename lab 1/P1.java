c5dd95323da2309c52d9714908059e4481cc2a71
Yash Raj <66545565+ryash072007@users.noreply.github.com>
2026-02-04 16:11:58 +0800

    lab 1 p1

---
+++ lab 1/P1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P1
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your choice: ");
+        String choice = scanner.next();
+
+        switch (choice){
+            case "A", "a":
+                System.out.println("Action Movie Fan");
+                break;
+            case "C", "c":
+                System.out.println("Comedy Movie Fan");
+                break;
+            case "D", "d":
+                System.out.println("Drama Movie Fan");
+                break;
+            
+            default:
+                System.out.println("Invalid Choice");
+        }
+    }
+}
